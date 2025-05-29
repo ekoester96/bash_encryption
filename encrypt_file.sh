9e497f3d6924a014904020352d615309c632f120
ekoester96 <ethan.schmidt1996@gmail.com>
2025-05-29 11:08:31 -0500

    Update encrypt_file.sh

--- encrypt_file.sh
+++ encrypt_file.sh
@@ -4,6 +4,8 @@ echo "listing files in this directory"
 ls -al
 
 read -p "which file would you like to encrypt?: " file
+
+# -pbkdf2 will prompt for password and will be needed to decrypt file
 openssl enc -aes-256-cbc -pbkdf2 -in "$file" -out encrypted_"$file"
 
 echo "$file encrypted as encrypted_$file"
